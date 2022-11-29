a8c2f3aa48123f4129bfcc720d83917f96b26aa1
ghgamble <grace.h.gamble@gmail.com>
2022-11-29 11:29:56 -0700

    adjust to async call

--- app/public/wp-content/themes/fictional-university-theme/build/index.asset.php
+++ app/public/wp-content/themes/fictional-university-theme/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => '318bd8372eb302abf92b');
+<?php return array('dependencies' => array('jquery'), 'version' => 'd449c3c203fb5361371f');
